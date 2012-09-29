9391feca516a2a0bc12180578ffa23b8ce9dea20
Diego Elio Pettenò <flameeyes@flameeyes.eu>
2012-09-29 07:31:26 -0700

    build: implement autotools build system for libopusfile. (v4)
    Includes
    - A make debug target that disables optimizations and enables
       assertions,
    - Proper ./configure switches for the optional features,
    - A configuration summary,
    - libtool versioning information,
    - Visibility and warning flags,
    - API documentation, and
    - Support for out-of-tree builds.
    
    Signed-off-by: Diego Elio Pettenò <flameeyes@flameeyes.eu>

---
+++ autogen.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Run this to set up the build system: configure, makefiles, etc.
+set -e
+
+package="opusfile"
+
+ACLOCAL_FLAGS="-I m4"
+
+olddir=`pwd`
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+cd "$srcdir"
+
+echo "Updating build configuration files for $package, please wait...."
+autoreconf -is
