7579e03fd2053482f1ff96e85032de3249c84760
Ralph Giles <giles@mozilla.com>
2012-10-24 15:04:39 -0700

    Update git-version for the vN.M release tagging scheme.
    We need the sed invocation to strip the 'v' prefix
    when naming the dist tarball.

--- doc/git-version.sh
+++ doc/git-version.sh
@@ -2,7 +2,8 @@
 
 # script to build a version string
 
-GIT_VERSION=$(git describe --tags --dirty 2> /dev/null)
+GIT_VERSION=$(git describe --tags --match 'v*' --dirty 2> /dev/null)
+GIT_VERSION=$(echo ${GIT_VERSION} | sed 's/^v//')
 if test -z ${GIT_VERSION}; then
   VERSION='unknown'
 else
