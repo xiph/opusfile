a6e628793ee1659d1f93ebcc5dbd3e52359d7a08
Ron <ron@debian.org>
2012-11-11 22:28:18 +1030

    Trim some things out of autogen.sh that aren't doing anything
    Mostly so that people looking at or cloning it later don't get confused
    into thinking that they are.
    
    Signed-off-by: Ralph Giles <giles@mozilla.com>

--- autogen.sh
+++ autogen.sh
@@ -2,15 +2,10 @@
 # Run this to set up the build system: configure, makefiles, etc.
 set -e
 
-package="opusfile"
+srcdir=`dirname $0`
+test -n "$srcdir" && cd "$srcdir"
+
+echo "Updating build configuration files for opusfile, please wait...."
 
 ACLOCAL_FLAGS="-I m4"
-
-olddir=`pwd`
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
-
-cd "$srcdir"
-
-echo "Updating build configuration files for $package, please wait...."
 autoreconf -isf
