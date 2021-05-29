112b79bbb03927b12535d1b01da4d3b8cfe18670
Hendrik <git@h3ndrk.de>
2021-05-29 13:09:48 +0200

    Include targets from all configurations

--- opusfileConfig.cmake
+++ opusfileConfig.cmake
@@ -4,5 +4,11 @@ find_dependency(
   Opus
 )
 
-# TODO: include(...opusfileTargets-debug.cmake)?
 include("${CMAKE_CURRENT_LIST_DIR}/opusfileTargets.cmake")
+
+# Load information for each installed configuration.
+get_filename_component(_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+file(GLOB CONFIG_FILES "${_DIR}/opusfileTargets-*.cmake")
+foreach(f ${CONFIG_FILES})
+  include(${f})
+endforeach()
