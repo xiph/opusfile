024cad828e20a9fe5354b7418fdace45e60b348e
Andy Kent <andy@binaryalchemy.net>
2021-08-16 08:42:42 +0100

    map channels for mapping family 255

--- src/info.c
+++ src/info.c
@@ -80,7 +80,31 @@ int opus_head_parse(OpusHead *_head,const unsigned char *_data,size_t _len){
   }
   /*General purpose players should not attempt to play back content with
      channel mapping family 255.*/
-  else if(head.mapping_family==255)return OP_EIMPL;
+  else if(head.mapping_family==255)
+  {
+    size_t size;
+    int ci;
+    if (head.channel_count != 16)
+      return OP_EBADHEADER;
+    size = 21 + head.channel_count;
+    if (_len < size || head.version <= 1 && _len > size)
+      return OP_EBADHEADER;
+    head.stream_count = _data[19];
+    if (head.stream_count < 1)
+      return OP_EBADHEADER;
+    head.coupled_count = _data[20];
+    if (head.coupled_count > head.stream_count)
+      return OP_EBADHEADER;
+    for (ci = 0; ci < head.channel_count; ci++)
+    {
+      if (_data[21 + ci] >= head.stream_count + head.coupled_count && _data[21 + ci] != 255)
+      {
+        return OP_EBADHEADER;
+      }
+    }
+    if (_head != NULL)
+      memcpy(_head->mapping, _data + 21, head.channel_count);
+  }
   /*No other channel mapping families are currently defined.*/
   else return OP_EBADHEADER;
   if(_head!=NULL)memcpy(_head,&head,head.mapping-(unsigned char *)&head);
